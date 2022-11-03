9a38448f87a3c9ae1d6867b4d22e6f5a6e07ff8c
Tim <timon-69@tut.by>
2022-11-03 17:20:58 +0100

    capitalize search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def index
     v = request.query_parameters
     if v[:name]
-      users = User.where(name: params[:name])
+      users = User.where(name: params[:name].capitalize)
       render json: users, status: 200
     else
       render 'index'
