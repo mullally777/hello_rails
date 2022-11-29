9c52ac902564cbdf8a50f706102a5ba75f59804c
Tim <timon-69@tut.by>
2022-11-29 10:32:01 +0100

    ff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -85,7 +85,7 @@ class UsersController < ApplicationController
     end
 
     def auth?
-      #return false unless cookies[:session_id] == "0ok9ij8uh?"
+      return false unless cookies[:session_id] == "0ok9ij8uh?"
       true
       #return false unless request.headers["token"].present?
       #return true if request.headers["token"] == "0ok9ij8uh?"
